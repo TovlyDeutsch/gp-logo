50c017b818903c4a32375a03ed84a174221b35f3
Tovly Deutsch <tovlydeutsch@gmail.com>
2020-03-23 12:31:41 -0400

    addec vectorizing func

--- plot.js
+++ plot.js
@@ -28,7 +28,7 @@ var x = d3
   .range([0, w]);
 var y = d3
   .scaleLinear()
-  .domain([0, 35])
+  .domain([0, 45])
   .range([h, 0]);
 
 function lineGen(xData, yData) {
@@ -61,7 +61,7 @@ function plot(xData, yData) {
   }
 }
 
-let activeLetter = "v";
+let activeLetter = "T";
 
 async function main() {
   let initialDataY;
